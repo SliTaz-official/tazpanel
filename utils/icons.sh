2a53bcdda3e6191ea1508f345739e4caa65a47ce
Christophe Lincoln <pankso@slitaz.org>
2011-04-05 01:32:41 +0200

    Add utils/icons.sh helper script to resize panel icons

---
+++ utils/icons.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# Use imagemagick to convert icon images fo TazPanel
+#
+
+STYLE=$1
+IMAGES=../styles/$STYLE/images
+SIZE=16x16
+
+[ -z "$1" ] && exit 0
+cd $IMAGES
+
+echo ""
+for i in *.png
+do
+	echo "convert $i -resize $SIZE $i"
+	convert $i -resize $SIZE $i
+done
+echo ""
+
+exit 0
